6571a1f9011c5ea1684c07dbaa81c6e0103b14c4
ateistsheep1997 <laguayonasu2fz@hotmail.com>
2025-09-03 19:07:33 +0200

    Add logging middleware for request method and URL

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.static('client/build'));
 
+// Logging middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.get('/api/data', (req, res) => {
     // Sample health data
     const healthData = {
