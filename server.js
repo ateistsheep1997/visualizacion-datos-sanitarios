05537b7aa9bcede7c9939f50e345501a45f5a7e6
ateistsheep1997 <laguayonasu2fz@hotmail.com>
2025-09-03 19:06:00 +0200

    Add CORS support for API requests

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.static('client/build'));
 
 app.get('/api/data', (req, res) => {
