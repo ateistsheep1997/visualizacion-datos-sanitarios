dc719f9696055473f664b423f07394a5bd3c7618
ateistsheep1997 <laguayonasu2fz@hotmail.com>
2025-09-03 19:03:10 +0200

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Visualización de Datos Sanitarios</h1>
+      {/* Componentes de visualización de datos irían aquí */}
+    </div>
+  );
+}
+
+export default App;
